c41f99cb6423e53d24e23ff0a58a258443bd137e
Beeny <129210955+beenycool@users.noreply.github.com>
2025-05-26 12:42:40 +0100

    fix #2

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const path = require('path');
 
 const nextConfig = {
   reactStrictMode: true,
+  output: process.env.IS_STATIC_EXPORT === 'true' ? 'export' : undefined,
   images: {
     unoptimized: true
   },
